5c07dd2390cd77bd54d1155ac0ab619795522871
Lee Saville <l.w.saville@gmail.com>
2017-03-31 16:15:42 -0600

    Add instructions and beginning of class

--- in_flight_entertainment_test.rb
+++ in_flight_entertainment_test.rb
@@ -1,4 +1,10 @@
 require 'minitest/autorun'
+require './in_flight_entertainment'
 
 class TestInFlightEntertainment < Minitest::Test
+  def test_it_exists
+    meow = InFlightEntertainment.new
+
+    assert_equal meow.class, InFlightEntertainment
+  end
 end
