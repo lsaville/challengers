a6e9f9e39009704252a84603839bdbf931f2bbf7
Lee Saville <l.w.saville@gmail.com>
2017-03-31 16:26:00 -0600

    Change variable name. Add test for the same movie twice

--- in_flight_entertainment_test.rb
+++ in_flight_entertainment_test.rb
@@ -3,13 +3,19 @@ require './in_flight_entertainment'
 
 class TestInFlightEntertainment < Minitest::Test
   def test_it_exists
-    meow = InFlightEntertainment.new(123, [34,53])
-    assert_equal meow.class, InFlightEntertainment
+    flight = InFlightEntertainment.new(123, [34,53])
+    assert_equal flight.class, InFlightEntertainment
   end
 
   def test_it_returns_true_given_two_values_that_equal_flight_length
-    meow = InFlightEntertainment.new(100, [40,60])
+    flight = InFlightEntertainment.new(100, [40,60])
 
-    assert meow.two_movies_fit?
+    assert flight.two_movies_fit?
+  end
+
+  def test_it_returns_false_given_the_same_movie_twice
+    flight = InFlightEntertainment.new(100, [50,50])
+
+    refute flight.two_movies_fit?
   end
 end
