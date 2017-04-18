bee79aa44d8567bc9a3b50221ec4734d525b6c87
Lee Saville <l.w.saville@gmail.com>
2017-04-18 14:55:31 -0600

    Mimic the style of the books solution

--- cracking_the_coding_interview/chapter_1/1_2_check_permutation.rb
+++ cracking_the_coding_interview/chapter_1/1_2_check_permutation.rb
@@ -44,7 +44,33 @@ def char_count(input)
   end
 end
 
-puts permutation('meow', 'woem')
-puts permutation('blahdls', 'ejlfsg')
-puts permutation('lalala', 'alalal')
-puts permutation('im a fine fellow', 'fellow fine im a')
+# Stab at doing it array style (without the array default overwrite in array_mimic.rb)
+
+def permutation(input_one, input_two)
+  return false if input_one.length != input_two.length
+
+  letters = Array.new
+  for char_num in input_one.codepoints do
+    if letters[char_num]
+      letters[char_num] += 1
+    else
+      letters[char_num] = 1
+    end
+  end
+
+  for test_char in input_two.codepoints do
+    if letters[test_char]
+      letters[test_char] -= 1
+    else
+      letters[test_char] = -1
+    end
+    return false if letters[test_char] < 0
+  end
+  return true
+end
+
+puts permutation('meow', 'woem') # Same word scrambled
+puts permutation('blahdls', 'ejlfsg') # Different lengths
+puts permutation('qwert', 'asdfg') # Same length different letters
+puts permutation('lalala', 'alalal') # Same scrambled again
+puts permutation('im a fine fellow', 'fellow fine im a') # Same scrambled with spaces
